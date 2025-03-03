9b7f0cd8c43a5f4e9b8da5386bef135bfde31ffa
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2025-03-04 04:12:22 +0530

    Add files via upload

---
+++ 125. Valid Palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+        bool isPalindrome(string s) {
+            
+            int n = s.length();
+    
+            int i = 0, j = n-1;
+            while(i < j){
+                
+                if(! isalnum(s[i]) )
+                { 
+                    i++;
+                    continue;
+                }
+                if(! isalnum(s[j])  ) 
+                {
+                    j--;
+                    continue;
+                }
+    
+                if( tolower(s[i]) != tolower(s[j]) )    return false;
+                else{
+                i++;
+                j--;
+                }
+            }
+            return true;
+        }
+    };
