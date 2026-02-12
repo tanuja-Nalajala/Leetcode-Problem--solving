9bcee9163d10d757cd6608bb2a6796bf1592f2c3
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-02-11 19:36:30 -0800

    Time: 2 ms (99.17%) | Memory: 46.9 MB (81.03%) - LeetSync

---
+++ neetcode/1-two-sum/two-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // approach - 1: take a hashmap to store numbers with indexes. 
+        // checks if target- curr is already in the list; -> return curr.key and taget-curr.key else, push to map;
+        //Time complexity : n
+        //space: n -> worst
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int n = nums.length;
+        for(int i = 0; i < n; ++i){
+            int curr = nums[i];
+            int remain = target - nums[i];
+            if(mp.containsKey(remain)){
+                return new int[] { mp.get(remain), i};
+            }
+                mp.put(curr, i);
+        }
+        return new int[] {-1 -1};
+        
+    }
+}
