4e1b6b311875a07eb1c656d7e57d1219e6b4f2b8
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-10 12:32:14 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (85.83%) - LeetSync

--- neetcode/70-climbing-stairs/climbing-stairs.java
+++ neetcode/70-climbing-stairs/climbing-stairs.java
@@ -10,7 +10,13 @@ class Solution {
     public int climbStairs(int n) {
     //recursion
             int []dp = new int [n + 1];
-            for(int i = 0; i < n; ++i) Arrays.fill(dp,-1);
-            return solve(n, dp);
+            Arrays.fill(dp,-1);
+            //return solve(n, dp);
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2; i <= n; ++i ){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
     }
 }
