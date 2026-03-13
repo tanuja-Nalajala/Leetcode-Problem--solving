10f8512c088f306981331578734ca681b039b3a0
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-12 23:22:40 -0700

    Time: 3 ms (26.16%) | Memory: 47.1 MB (10.95%) - LeetSync

--- neetcode/695-max-area-of-island/max-area-of-island.java
+++ neetcode/695-max-area-of-island/max-area-of-island.java
@@ -1,8 +1,8 @@
 class Solution {
-    int DFS(int [][]grid, int i, int j, int m, int n, boolean [][] visited){
-        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j] == true || grid[i][j] != 1 ) return 0;
+    int DFS(int [][]grid, int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != 1 ) return 0;
 
-        visited[i][j] = true;
+        grid[i][j] = 0;
         int [][] dir = {
             {1,0}, {0,1}, {-1, 0}, {0,-1}
         };
@@ -10,7 +10,7 @@ class Solution {
         for(int[] d : dir){
             int nr = i + d[0];
             int nc = j + d[1];
-            ans += DFS(grid, nr, nc, m, n, visited);
+            ans += DFS(grid, nr, nc, m, n);
         }
         return ans;
 
@@ -26,7 +26,7 @@ class Solution {
         for(int i = 0; i < rows; ++i){
             for(int j = 0; j < cols; ++j){
                 if(grid[i][j] == 1 && visited[i][j] != true){
-                    int area = DFS(grid, i, j, rows, cols, visited);
+                    int area = DFS(grid, i, j, rows, cols);
                     maxArea = Math.max(area, maxArea);
                 }   
             }
