20841d653f055cd271b62b79c8f8de7f82c3661a
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-13 14:37:07 -0700

    Time: 27 ms (27.21%) | Memory: 47 MB (29.45%) - LeetSync

--- neetcode/322-coin-change/coin-change.java
+++ neetcode/322-coin-change/coin-change.java
@@ -1,32 +1,27 @@
 class Solution {
-    int solve(int[] coins,int i, int amount, int[][] dp){
-        if(i == 0){
-            if(amount % coins[i] == 0) 
-                return amount / coins[i];
-            else return (int)1e9;
+    int solve(int[]coins, int ind, int amount, int[][]dp){
+        if(ind == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
+            }else{
+                return (int)1e9;
+            }
         }
 
-        if(dp[i][amount] != -1) return dp[i][amount];
-
-        int notPick = 0 + solve(coins,i-1, amount, dp);
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        //can chose to pick ith coin / or not
+        int notPick = 0 + solve(coins, ind - 1, amount, dp);
         int pick = Integer.MAX_VALUE;
-        if(coins[i] <= amount) pick = 1+ solve(coins, i, amount-coins[i], dp);
-        return dp[i][amount] = Math.min(pick , notPick);
+        //can pick a coin only if it is less than amount
+        if(coins[ind] <= amount) pick = 1 + solve(coins, ind, amount - coins[ind],dp);
+        return dp[ind][amount] = Math.min(pick, notPick);
     }
     public int coinChange(int[] coins, int amount) {
-        
         int n = coins.length;
-
-        int [][] dp = new int[n][amount + 1];
-        for(int i = 0; i< n; ++i) Arrays.fill(dp[i], -1);
-
-        if(amount == 0 ) return 0;
-
-        for(int i = 0; i< n; ++i){
-            dp[i][0] = 0;
-        }
-        int ans = solve(coins,n-1, amount, dp);
-        return (ans != 1e9)? ans : -1;  
-        
+        if(amount == 0) return 0;
+        int[][] dp = new int[n][amount + 1];
+        for(int i = 0; i < n; ++i) Arrays.fill(dp[i], -1);
+        int ans = solve(coins, n-1,amount,dp);
+        return ans == 1e9 ? -1 : ans;
     }
 }
