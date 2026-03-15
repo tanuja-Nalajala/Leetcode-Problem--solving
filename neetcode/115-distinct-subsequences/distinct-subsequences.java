f7821ec09fd204c9d2c106221c03134e4c608fb6
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-15 12:22:34 -0700

    Time: 20 ms (52.71%) | Memory: 54.4 MB (47.47%) - LeetSync

--- neetcode/115-distinct-subsequences/distinct-subsequences.java
+++ neetcode/115-distinct-subsequences/distinct-subsequences.java
@@ -19,20 +19,23 @@ class Solution {
         int n = t.length();
 
         int [][] dp = new int [m+1][n+1];
-        for(int i = 0; i < m; ++i) Arrays.fill(dp[i], -1);
-        return solve(s,t,0, 0, m, n, dp);
-        // for(int i = 0 ; i < m; ++i){
-        //     dp[i]
-        // }
-        // for(int i = m-1; i >= 0; ++i){
-        //     for(int j = n-1; j >= 0; ++j){
-        //         if(s[i] == t[j]){
-        //             //chose s[i] to be part of t or not 
-        //             pick = dp[i-1][j-1];  
-        //         }
-        //         notPick = dp[i-1][j]
+        // for(int i = 0; i < m; ++i) Arrays.fill(dp[i], -1);
+        // return solve(s,t,0, 0, m, n, dp);
 
-        //     }
-        // }
+        for(int i = 0 ; i <= m; ++i){
+            dp[i][n] = 1;
+        }
+        
+
+        for(int i = m - 1; i >= 0; --i){
+            for(int j =  n - 1; j >= 0; --j){
+                int pick = 0, notPick = 0;
+                if(i < m && s.charAt(i) == t.charAt(j))
+                        pick =  dp[i  + 1][j + 1];
+                notPick =  dp[i + 1][j];
+                dp[i][j] = pick + notPick;
+            }
+        }
+        return dp[0][0];
     }
 }
