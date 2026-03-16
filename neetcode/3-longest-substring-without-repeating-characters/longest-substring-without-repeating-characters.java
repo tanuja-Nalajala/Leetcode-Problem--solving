714ed0e36ba284c5f3573939c86cd5aa7eb3d005
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-16 13:22:34 -0700

    Time: 5 ms (86.76%) | Memory: 45.7 MB (72.12%) - LeetSync

--- neetcode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ neetcode/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,31 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        //characters: A_Z?
-        //empty string? possible? yes -> return value?0:-1
-        //approach: lets starts with 1 lenth substring -> increase pointer until existing /seen character surfaces? update curr longest as max; update intial pointer to past(index+1) of seen char;
-
-        // time complexity: O(n), SC: i need a array of 100 to track sysmbols
-
         int n = s.length();
-        HashMap<Character, Integer> mp = new HashMap<>();
-        if(n == 0) return 0;
-        //System.out.println(n);
-
-        int i = 0, j = 0;
-        int ans = 0, local_max = 0;
-        while(j < n ){
-            char curr = s.charAt(j);
-            if(mp.containsKey(curr) && mp.get(curr) >= i){
-                int last_seen = mp.get(curr);
-                i = last_seen + 1;
+        Map<Character, Integer> mp = new HashMap<>();
+        int j = 0, maxLength = 0;
+        for(int i = 0; i < n; ++i){
+            
+            if(mp.containsKey(s.charAt(i))){
+                j = Math.max(j, mp.get(s.charAt(i)) + 1);
             }
-            mp.put(curr, j);
-            //local_max++;
-            ans = Math.max(ans, j - i + 1);
-            j++;
+            mp.put(s.charAt(i), i);
+            maxLength = Math.max(maxLength, i-j+1);
         }
-        return ans;
-        
+        return maxLength;
     }
 }
