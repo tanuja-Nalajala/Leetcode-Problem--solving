49bab2cf20fe1db293fbed8cab56823c85026912
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-18 16:35:38 -0700

    Time: 4 ms (98.41%) | Memory: 85.4 MB (80.89%) - LeetSync

--- neetcode/1522-stone-game-iii/stone-game-iii.java
+++ neetcode/1522-stone-game-iii/stone-game-iii.java
@@ -16,12 +16,31 @@ class Solution {
         int n = stoneValue.length;
 
         //dp[i] => represents max_possible difference b/w Alice and bob, if alice pick (1 to 3) stones in ith turn
-        int [] dp = new int[n+ 1];
-            Arrays.fill(dp, -1);
-        int ans = solve(stoneValue, 0, n, dp);
-        if(ans > 0) 
+        int [] dp = new int[n+ 3];
+            //Arrays.fill(dp, -1);
+        // int ans = solve(stoneValue, 0, n, dp);
+        // if(ans > 0) 
+        //     return "Alice";
+        // else if(ans == 0) 
+        //     return "Tie";
+        // else
+        //      return "Bob";
+
+        for(int  ind = n-1; ind >= 0; --ind){
+            //if alice picks stone-1 => alice-count - (max_count of bob can make with remaining stones)
+              int ans = Integer.MIN_VALUE;
+                ans = Math.max(ans, stoneValue[ind] - dp[ind + 1] );
+            //if alice picks 2stones => alice-count - (max_count bob can make with remaining stones)
+            if(ind + 1 < n) 
+                ans = Math.max( ans, (stoneValue[ind] + stoneValue[ind + 1]) - dp[ind + 2] );
+            if(ind + 2 < n) 
+                ans  = Math.max(ans, (stoneValue[ind] + stoneValue[ind + 1] + stoneValue[ind + 2]) - dp[ind + 3] );
+
+            dp[ind] = ans;
+        }
+        if(dp[0] > 0) 
             return "Alice";
-        else if(ans == 0) 
+        else if(dp[0] == 0) 
             return "Tie";
         else
              return "Bob";
