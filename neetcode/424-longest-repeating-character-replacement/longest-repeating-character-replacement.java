5a1d7fbe8d1477cae987303a53afb785d7eb04a6
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-19 15:28:58 -0700

    Time: 8 ms (69.89%) | Memory: 46 MB (87.71%) - LeetSync

--- neetcode/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ neetcode/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,40 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        
         int n = s.length();
-        // int i = 0, j  = 0;
-        // length = 0, max_freq = 0
-        // if lentgh - maxfreq <= k ==> valid 
-        // max_length = length;
-        // if(length - max freq > k) ==> needs to more than k changes
-        // I need to move my window
-        // i will keep moving my window untill i found a valid length string (lentgh - maxfreq <= k)
 
-        //needs to keep track of max_freq
-        int [] freq = new int[26];
-        int length = 0, max_length = 0, max_freq = 0;
-        int r = 0, l = 0;
+        int l = 0, r = 0, length = 0, max_freq = 0, start_idx = 0, max_length = 0;
+        int []hash = new int[256];
         while(r < n){
-            freq[s.charAt(r) - 'A'] = freq[s.charAt(r) - 'A'] + 1;
-            max_freq = Math.max(max_freq, freq[s.charAt(r) - 'A']);
-            length = r - l + 1;
-            //invalid
-            //length = 5 max_freq = 2
-            while(length - max_freq > k){
-                //update window ==> l++
-                //decrement freq of char at l
-                //also find new max_freq
-                freq[s.charAt(l)-'A']--;
-                 max_freq = 0;
-                for(int i = 0; i < 26; ++i){
-                   max_freq = Math.max(max_freq, freq[i]);
+            char rc = s.charAt(r);
+            hash[rc - 'A']++;
+            max_freq = Math.max(max_freq, hash[rc - 'A']);
+            
+            while((r - l + 1) - max_freq > k){
+                char lc = s.charAt(l);
+                hash[lc - 'A']--;
+                max_freq = 0;
+                for (int c = 0; c < 26; c++) {
+                    max_freq = Math.max(max_freq, hash[c]);
                 }
                 l++;
-                length = r - l + 1;
             }
-            max_length = Math.max(max_length, length);
-
-        r++;    
+            if((r - l + 1) - max_freq <= k)
+                max_length = Math.max(max_length, r - l + 1);
+            
+        r++;
         }
         return max_length;
     }
