a985362ca65b73f0da58b2b4674d01bc3393f723
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-19 18:00:45 -0700

    Time: 53 ms (79.62%) | Memory: 70.2 MB (74.06%) - LeetSync

--- neetcode/940-fruit-into-baskets/fruit-into-baskets.java
+++ neetcode/940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,20 +1,23 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> mp = new HashMap<>();
+        
         int n = fruits.length;
-        int l = 0,  r = 0, max_length = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int l = 0, r = 0, ans = 0;
         while(r < n){
-            mp.put(fruits[r], mp.getOrDefault(fruits[r], 0)+ 1);
+            mp.put(fruits[r], mp.getOrDefault(fruits[r], 0)+1 );
             while(mp.size() > 2){
+                //here my window is invald
+                //decrement count of frfuit at l
                 mp.put(fruits[l], mp.get(fruits[l])-1 );
                 if(mp.get(fruits[l]) == 0){
                     mp.remove(fruits[l]);
                 }
                 l++;
             }
-            max_length = Math.max(max_length, r-l+1);
+            ans = Math.max(ans, r - l + 1);
         r++;
         }
-        return max_length;
+        return ans;
     }
 }
