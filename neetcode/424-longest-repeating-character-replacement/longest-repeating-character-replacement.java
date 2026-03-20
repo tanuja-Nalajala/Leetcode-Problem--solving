13c810f4569a82fc2a6fdd87e3170ea40dcefdff
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-20 12:06:39 -0700

    Time: 6 ms (98.95%) | Memory: 46.6 MB (30.65%) - LeetSync

--- neetcode/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ neetcode/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,27 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        int l = 0, r = 0, length = 0, maxFreq = 0, maxlength = 0;
+        int [] hash = new int[256];
         int n = s.length();
-
-        int l = 0, r = 0, length = 0, max_freq = 0, start_idx = 0, max_length = 0;
-        int []hash = new int[26];
         while(r < n){
             char rc = s.charAt(r);
             hash[rc - 'A']++;
-            max_freq = Math.max(max_freq, hash[rc - 'A']);
-            
-            while((r - l + 1) - max_freq > k){
+            maxFreq = Math.max(maxFreq, hash[rc - 'A']);
+            while(r - l + 1 - maxFreq > k){
                 char lc = s.charAt(l);
                 hash[lc - 'A']--;
-                max_freq = 0;
-                for (int c = 0; c < 26; c++) {
-                    max_freq = Math.max(max_freq, hash[c]);
+                for(int i = 0; i < 26; ++i){
+                    maxFreq = Math.max(maxFreq, hash[i]);
                 }
-                l++;
+
+            l++;
             }
-            max_length = Math.max(max_length, r - l + 1);
-            
+            maxlength = Math.max(maxlength, r - l + 1);
         r++;
         }
-        return max_length;
+        return maxlength;
     }
 }
