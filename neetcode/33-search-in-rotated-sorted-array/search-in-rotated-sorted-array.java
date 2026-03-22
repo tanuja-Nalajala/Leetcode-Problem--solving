7deba6dc7ad140895ba2daafa620791f74482ee5
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-22 12:16:19 -0700

    Time: 0 ms (100.00%) | Memory: 43.8 MB (59.59%) - LeetSync

---
+++ neetcode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        //
+
+        int n = nums.length;
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid = (l + (r - l)/2);
+            if(nums[mid] == target)
+                return mid;
+            //check for sorted half
+            if(nums[mid] <= nums[r] ){
+                if(target <= nums[r] && target >= nums[mid])
+                    l = mid + 1;
+                else r = mid - 1;
+            }
+            else{
+                if(target >= nums[l] && target <= nums[mid])
+                    r = mid -1;
+                else
+                    l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
