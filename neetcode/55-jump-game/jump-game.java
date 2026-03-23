bbd93f791c4739d4ec4224a65efa396376763141
Thanuja Nalajala <141996344+tanuja-Nalajala@users.noreply.github.com>
2026-03-23 13:06:01 -0700

    Time: 2 ms (85.53%) | Memory: 47.9 MB (73.96%) - LeetSync

--- neetcode/55-jump-game/jump-game.java
+++ neetcode/55-jump-game/jump-game.java
@@ -1,13 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-
-        //at each index we check what max_ind i can jummp to
-        // if i am at ith index which is greater than max_ind: means i know that ith index is not reachable
-
         int max_length = 0;
-        for(int i = 0; i < n; ++i){
-
+        for(int i = 0; i < nums.length; ++i){
             if(i > max_length) return false;
             max_length = Math.max(max_length, i + nums[i]);
         }
